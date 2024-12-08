2767fc756f7339dbe755bb25e763ac1363802fff
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-12-08 20:34:54 +0800

    feat: added start button functionality

--- src/index.js
+++ src/index.js
@@ -9,12 +9,11 @@ import { Player } from "./factories";
   userPlayer.gameboard.placeShips();
   computerPlayer.gameboard.placeShips();
 
-  btnListeners(userPlayer);
+  generateBtn();
+  btnListeners(userPlayer, computerPlayer);
 
   generateCells(".player-field", userPlayer.type);
-  // generateCells(".computer-field", "computer");
   showShips(userPlayer.gameboard.getAllShipCoordinates(), "human");
-  // loadListeners(userPlayer.gameboard, computerPlayer.gameboard);
 })();
 
 export function checkWinner(gb1, gb2) {
@@ -33,17 +32,54 @@ export function checkWinner(gb1, gb2) {
   }
 }
 
-function btnListeners(userPlayer) {
+function generateBtn() {
+  const container = document.querySelector("#container>div");
+  const div = document.createElement("div");
+  const btn = document.createElement("button");
+  btn.setAttribute("type", "button");
+
+  div.id = "button-container";
+
+  const placementBtn = btn.cloneNode();
+  placementBtn.id = "change-placement";
+  placementBtn.textContent = "Change Placement";
+
+  const startBtn = btn.cloneNode();
+  startBtn.id = "start-btn";
+  startBtn.textContent = "Start Game";
+
+  container.appendChild(div);
+  div.appendChild(placementBtn);
+  div.appendChild(startBtn);
+}
+
+function generateEnemyContainer() {
+  const container = document.querySelector("#container");
+  const div = document.createElement("div");
+  const h1 = document.createElement("h1");
+  const cellContainer = div.cloneNode();
+
+  h1.textContent = "Enemy Fleet";
+  container.appendChild(div);
+  div.appendChild(h1);
+
+  cellContainer.classList.add("computer-field");
+  cellContainer.classList.add("field-container");
+
+  div.appendChild(cellContainer);
+}
+
+function btnListeners(userPlayer, computerPlayer) {
   document.querySelector("#new-game").addEventListener("click", () => {
     document.querySelector("#winner").close();
   });
-  //prevent closing modal with escape btn
+  //prevent closing dialog with escape btn
   document.querySelector("#winner").addEventListener("keydown", (event) => {
     if (event.key === "Escape") {
       event.preventDefault();
     }
   });
-
+  //change placement btn listener
   document.querySelector("#change-placement").addEventListener("click", () => {
     userPlayer = Player("player", "human");
     const allCells = document.querySelectorAll(".human");
@@ -53,5 +89,12 @@ function btnListeners(userPlayer) {
     generateCells(".player-field", userPlayer.type);
     userPlayer.gameboard.placeShips();
     showShips(userPlayer.gameboard.getAllShipCoordinates(), "human");
+  });
+  //start btn listener
+  document.querySelector("#start-btn").addEventListener("click", () => {
+    generateEnemyContainer();
+    generateCells(".computer-field", computerPlayer.type);
+    loadListeners(userPlayer.gameboard, computerPlayer.gameboard);
+    document.querySelector("#button-container").remove();
   });
 }
